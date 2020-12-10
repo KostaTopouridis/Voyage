cf211dffcd002709aa74d9de1678e09395820afd
Kosta Topouridis <k.topouridis@gmail.com>
2020-12-10 23:39:44 +0100

    Beginning with login

--- Assets/Scripts/PostManager.cs
+++ Assets/Scripts/PostManager.cs
@@ -9,22 +9,16 @@ public class PostManager : MonoBehaviour
     public GameObject post;
     public TMP_InputField message;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void NewPost()
     {
-        GameObject newPost = Instantiate(post);
-        newPost.transform.SetParent(postPanel, false);
-        newPost.GetComponent<Post>().message = message.text;
+        if (message.text == string.Empty)
+        {
+            print("Nothing has been written");
+        } else
+        {
+            GameObject newPost = Instantiate(post);
+            newPost.transform.SetParent(postPanel, false);
+            newPost.GetComponent<Post>().message = message.text;
+        }
     }
 }
---
+++ Assets/Scripts/ShowHidePassword.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShowHidePassword : MonoBehaviour
+{
+    public TMP_InputField password;
+
+    public void ShowHide(bool isOn)
+    {
+        if (GetComponent<Toggle>().isOn)
+        {
+            password.contentType = TMP_InputField.ContentType.Standard;
+            password.DeactivateInputField();
+            password.ActivateInputField();
+        } else
+        {
+            password.contentType = TMP_InputField.ContentType.Password;
+            password.DeactivateInputField();
+            password.ActivateInputField();
+        }
+    }
+}
