f06a29d6f67607a7185e0684ec15d428d5228f4a
Kosta Topouridis <k.topouridis@gmail.com>
2020-12-10 17:03:57 +0100

    Initial Commit

---
+++ Assets/Scripts/HideReport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HideReport : MonoBehaviour
+{
+    void OnValueChanged()
+    {
+        if (GetComponent<TMP_Dropdown>().value == 0)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            print("Post reported!");
+        }
+    }
+}
---
+++ Assets/Scripts/RandomPostColor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomPostColor : MonoBehaviour
+{
+    public List<Color> pastelColors = new List<Color>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Image>().color = pastelColors[Random.Range(0,pastelColors.Count)];
+    }
+}
